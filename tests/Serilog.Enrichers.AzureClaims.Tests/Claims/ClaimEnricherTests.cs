b8f226052c6d1fbe0353278d6a97d0627b5fb715
Thorstein Løkensgard <thorstein.lokensgard@intility.no>
2024-11-22 13:19:34 +0100

    tests: add test for claim enricher where httpContext is null and log already exists

--- tests/Serilog.Enrichers.AzureClaims.Tests/Claims/ClaimEnricherTests.cs
+++ tests/Serilog.Enrichers.AzureClaims.Tests/Claims/ClaimEnricherTests.cs
@@ -5,6 +5,8 @@ using Serilog.Events;
 using System.Security.Claims;
 using Xunit;
 using Serilog.Enrichers.Claims;
+using Serilog.Core;
+using Serilog.Parsing;
 
 namespace Serilog.Enrichers.AzureClaims.Tests.Claims;
 
@@ -122,5 +124,62 @@ public class ClaimEnricherTests
         Assert.NotNull(evt);
         Assert.True(evt.Properties.ContainsKey(_customClaimType));
         Assert.Equal(TestConstants.EMAIL, evt.Properties[_customClaimType].LiteralValue().ToString());
+    }
+
+    [Fact]
+    public void Enrich_ShouldReturn_WhenHttpContextIsNull()
+    {
+        // Arrange
+        var httpContextAccessorMock = Substitute.For<IHttpContextAccessor>();
+        httpContextAccessorMock.HttpContext.Returns((HttpContext?)null);
+
+        var customClaimEnricher = new ClaimEnricher(httpContextAccessorMock, _customClaimType, _customClaimPropertyName);
+        var logEvent = new LogEvent(
+            DateTimeOffset.Now,
+            LogEventLevel.Information, 
+            null, 
+            new MessageTemplate([]), []
+        );
+
+        var propertyFactory = Substitute.For<ILogEventPropertyFactory>();
+
+        // Act
+        customClaimEnricher.Enrich(logEvent, propertyFactory);
+
+        // Assert
+        Assert.Empty(logEvent.Properties);
+    }
+
+    [Fact]
+    public void Enrich_ShouldAddLogEventProperty_WhenItemKeyIsLogEventProperty()
+    {
+        // Arrange
+        var httpContextAccessorMock = Substitute.For<IHttpContextAccessor>();
+        var user = new ClaimsPrincipal(TestClaimsProvider.ValidClaims().GetClaimsPrincipal());
+
+        httpContextAccessorMock.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user,
+            Items = new Dictionary<object, object>()
+        });
+
+        var customClaimEnricher = new ClaimEnricher(httpContextAccessorMock, _customClaimType, _customClaimPropertyName);
+        var logEvent = new LogEvent(
+            DateTimeOffset.Now,
+            LogEventLevel.Information,
+            null,
+            new MessageTemplate([]), []
+        );
+
+        var propertyFactory = Substitute.For<ILogEventPropertyFactory>();
+
+        var logEventProperty = new LogEventProperty(_customClaimPropertyName, new ScalarValue("test@example.com"));
+        httpContextAccessorMock.HttpContext.Items[$"Serilog_{_customClaimPropertyName}"] = logEventProperty;
+
+        // Act
+        customClaimEnricher.Enrich(logEvent, propertyFactory);
+
+        // Assert
+        Assert.Contains(logEvent.Properties, p => p.Key == _customClaimPropertyName && p.Value.Equals(new ScalarValue("test@example.com")));
     }
 }
